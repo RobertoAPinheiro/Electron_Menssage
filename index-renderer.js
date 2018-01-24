9215e7a8746eeb7153320e6bb0d31dbeda9db051
Roberto Pinheiro <roberto.assad@inatel.br>
2018-01-24 17:01:49 -0200

    Connection to firebase Realtime Data base and creation the personal register modal

--- index-renderer.js
+++ index-renderer.js
@@ -10,26 +10,68 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Get a reference to the database service
+var database = firebase.database();
+var userId;
+var userEmail;
+var first_name;
+
+var finishRegistration = document.getElementById('btn_finish_registration');
+
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        console.log('User In');
         checkUserData();
     } else {
         document.location.href = 'login.html';
     }
 });
 
-function checkUserData(){
+function checkUserData() {
 
-    var userId = firebase.auth().currentUser.uid;
-    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-      var username = (snapshot.val() && snapshot.val().username);
-    
-      if(username == null){
-          console.log('null');
-          var modal = document.getElementById('myModal');
-          modal.style.display = "block";
+    userId = firebase.auth().currentUser.uid;
+    userEmail = firebase.auth().currentUser.email;
 
-      }
+    return firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
+        first_name = (snapshot.val() && snapshot.val().first_name);
+
+        if (first_name == null) {
+            var show_modal = document.getElementById('modal_registration');
+            show_modal.style.display = "block";
+
+        }
     })
+}
+
+var reg_modal = new Vue({
+    el: '#modal_registration',
+    data: {
+        exists: true,
+        user_register: {
+            username: '',
+            first_name: '',
+            last_name: '',
+            birthday: '',
+            phone: ''
+        }
+    },
+    methods: {
+        finishRegistration: function () {
+            writeUserData(userId, userEmail, this.user_register.first_name, this.user_register.last_name, this.user_register.birthday, this.user_register.phone);
+        }
+    }
+})
+
+function writeUserData(userId, userEmail, first_name, last_name, birthday, phone) {
+    firebase.database().ref('users/' + userId).set({
+        id: userId,
+        email: userEmail,
+        first_name: first_name,
+        last_name: last_name,
+        birthday: birthday,
+        phone: phone
+    });
+
+    firebase.database().ref('find_users_email/' + userEmail).set({
+        id: userId
+    });
 }
